e3113abd418aae21f7d16bd3332922bb7aab6c29
ST0927 <shigeyuki.taira@gmail.com>
2024-04-01 13:45:14 +0900

    画面表示のみだった項目をsendDataに追加 ToDo：アプリを閉じたり開いたりするとテキストかボタンかズレるのでグローバル変数で管理する必要がある

--- Chatapp/Chatapp/LoggerView.swift
+++ Chatapp/Chatapp/LoggerView.swift
@@ -57,16 +57,28 @@ struct Logger : View {
     @State var screenHeight:CGFloat = 0
     @State var tapPosition_x:CGFloat = 0
     @State var tapPosition_y:CGFloat = 0
+    @State var text_len:Int = 0
+    @State var text_len_ave:Double = 0
+    @State var response_time_ave:Double = 0
     func sendLoggerData() {
+        
+        //日付の設定
         let now = Date()
         let dateUnix: TimeInterval = now.timeIntervalSince1970
 
-        
         //HTTPPOSTの形式を指定
         let url = URL(string: "https://datalake.iopt.jp/v1/sensor_data")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if !message_len.isEmpty {
+            text_len = message_len[message_len.count - 1].count
+            text_len_ave = Double(message_len.joined().count)/Double(message_len.count)
+        }
+        if !ResponseTimeCounts.isEmpty {
+            response_time_ave = Double(ResponseTimeCounts.reduce(0, +))/Double(ResponseTimeCounts.count)
+        }
         
         //送信する内容のリスト
         let sendData = ["key": "t9eX8tyr7G_ZQk-2",
@@ -75,19 +87,25 @@ struct Logger : View {
                                  "sensor_id": userStore.email,
                                  "data_time": dateUnix,
                                 ],
-                        "body": ["event": event,
-                                 
+                        "body": ["event": event,//イベント名
+                                 //端末サイズ
                                  "screen_width":screenWidth,
                                  "screen_height":screenHeight,
-                                 
+                                 //タップ：カウント、頻度、場所
                                  "tap_count":tapNum,
                                  "tap_interval":TimeCount,
-                                 "tap_position_x":"",
-                                 "tap_position_y":"",
-                                 
+                                 "tap_position_x":tapPosition_x,
+                                 "tap_position_y":tapPosition_y,
+                                 //選択ボタン
                                  "choice_left":LeftChoice,
                                  "choice_right":RightChoice,
-                                 
+                                 //テキスト入力：長さ、平均長さ、削除回数、回答にかけた時間、
+                                 "text_len":text_len,
+                                 "text_len_ave":text_len_ave,
+                                 "text_delete_count":Delete,
+                                 "response_time":ResponseTimeCount,
+                                 "response_time_ave":response_time_ave,
+                                 //スクロール：発生位置（Y軸）、長さ、時間、速さ
                                  "view_position":abs(offsetY - initOffsetY),
                                  "scroll_length":abs(endposition - startposition),
                                  "scroll_time":ScrollingTime,
