3b959e02608cc156c4d901622c52bd0b94402a34
ST0927 <shigeyuki.taira@gmail.com>
2024-04-16 20:35:37 +0900

    アンケートが終わったらもう開けないようにした マルチユーザーで保証されてるかは要確認

--- Chatapp/Chatapp/QuestionListView.swift
+++ Chatapp/Chatapp/QuestionListView.swift
@@ -15,6 +15,34 @@ struct QuestionListView: View {
     }
 }
 
+struct TaskListView: View {
+    @State private var taskActive = false
+    @State private var isActive = false
+    @State private var showAlert = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink(destination: Talk().environmentObject(QuestionList()), isActive: $isActive) {
+                    Button("4/20~ プレ実験") {
+                        if taskActive {
+                            isActive = true
+                        } else {
+                            showAlert = true
+                        }
+                    }
+                }
+            }
+            .navigationTitle("実験リスト")
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("注意"), message: Text("アンケートは終了しました"), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+}
+
+
 #Preview {
-    QuestionListView()
+//    QuestionListView()
+    TaskListView()
 }
