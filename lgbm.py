f176cf1798455f155f7d27726e89e49a7f27d9ca
ST0927 <shigeyuki.taira@gmail.com>
2024-06-09 13:51:51 +0900

    test

---
+++ lgbm.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+file_path = '/Users/shigeyuki-t/Desktop/FirstExperiment/分析用/05.csv'
+dataset = pd.read_csv(file_path)
+print(dataset.head(3))
+dataset.info()
+dataset.describe()
+
+predict_param = dataset['isAnswerCorrect']
